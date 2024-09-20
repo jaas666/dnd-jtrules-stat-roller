ba47b5f909ac8524aa1fcbd913345c0543675eec
Juan A <30204147+jaas666@users.noreply.github.com>
2024-09-20 16:06:18 -0500

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import DiceRoller from "./components/DiceRoller";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <DiceRoller />
+      <Analytics />
     </div>
   );
 };
